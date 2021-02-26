5901e1d4581466fab326759989fa35e160c56a43
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-02-26 08:37:42 -0500

    Added DB directory, schema and seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Seed Burgers into Database
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("The Hangover", false),
+  ("Bacon Double Cheeseburger", false),
+  ("Classic Cheeseburger", false),
+  ("Baconator", false),
+  ("Quarter Pounder", false),
+  ("Royale with Cheese", false),
+  ("Double Triple", true);
