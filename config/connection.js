5157b59ed5760edf5be976aba74426c36dc78bd3
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-01 08:31:41 -0500

    Added dotenv to conceal pswd

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 // Node Dependency
 var mysql = require('mysql');
+const dotenv = require('dotenv');
 var connection;
+
+// Call dotenv configuration
+dotenv.config();
 
 // For Heroku Deployment vs. Local MySQL Database
 if(process.env.JAWSDB_URL){
@@ -10,7 +14,7 @@ else{
   connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'ilovetocode', // Add your password
+    password: process.env.password,
     database : 'burgers_db' // Add your database
   });
 }
